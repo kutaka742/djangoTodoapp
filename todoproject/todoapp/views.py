755c40366cc6e0474d7ea14fdf2207bf171e1d21
kubotatka <ku.taka.742@gmail.com>
2025-07-14 10:39:35 +0900

    各種機能・ページ追加

--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,6 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
+
+from todoapp.models import Task
 
 # Create your views here.
-def tasklist(request):
-    return HttpResponse("<h1>Hello Django</h1>")
+class Tasklist(ListView):
+    model = Task
+    context_object_name = "tasks"
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__" # ["user", "title", "description", ....]
+    success_url = reverse_lazy("tasks")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+class TaskDelete(DeleteView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
